1915d77e06726551532a036d2388957eea23181c
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2025-01-17 01:10:15 +0000

    more construct promotion

--- src/Configs/ImageConfig.php
+++ src/Configs/ImageConfig.php
@@ -2,31 +2,14 @@
 
 namespace Hlsxx\QrWatermark\Configs;
 
-class ImageConfig {
-
-  public int $pixelSize;
-  public int $marginSize;
-  public array $color;
-  public ?array $colorsGradient;
-  public array $backgroundColor;
-  public bool $isAutoGradientEnabled;
-
+class ImageConfig
+{
   public function __construct(
-    int $pixelSize,
-    int $marginSize,
-    array $color,
-    ?array $colorsGradient,
-    array $backgroundColor,
-    bool $isAutoGradientEnabled
-  ) {
-    $this->pixelSize = $pixelSize;
-    $this->marginSize = $marginSize;
-    $this->color = $color;
-    $this->colorsGradient = $colorsGradient;
-    $this->backgroundColor = $backgroundColor;
-    $this->isAutoGradientEnabled = $isAutoGradientEnabled;
-  }
-
+    public int $pixelSize,
+    public int $marginSize,
+    public array $color,
+    public ?array $colorsGradient,
+    public array $backgroundColor,
+    public bool $isAutoGradientEnabled
+  ) {}
 }
-
-?>
